ad4e2bbf8429c6c72bfc12732da8a6a119189b1d
Roosa Huttunen <huttunenroosa00@gmail.com>
2022-04-12 16:19:01 +0300

    more functionality

--- src/index.py
+++ src/index.py
@@ -1,4 +1,5 @@
 import pygame
+import time
 from speedtyping import SpeedTyping
 
 def main():
@@ -6,18 +7,57 @@ def main():
     display = pygame.display.set_mode((750, 500))
     pygame.display.set_caption("Speed Typing Test")
     screen = SpeedTyping()
-    sentence = screen.get_sentence()
-    font = pygame.font.SysFont("Times New Roman", 24)
-    text = font.render(sentence, True, (255,255,255))
+    blue = (0, 0, 255)
 
-    display.fill((0,0,0))
-    display.blit(text, (300, 250))
-    pygame.display.flip()
+    end = False
+    clock = pygame.time.Clock()
 
     while True:
+        clock.tick(60)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 exit()
+            if end == True:
+                if event.type == pygame.MOUSEBUTTONUP:
+                    x, y = pygame.mouse.get_pos()
+                    if x >= 325 and x <= 425 and y >= 385 and y <= 485:
+                        screen.reset()
+                        end = False
+            elif event.type == pygame.KEYDOWN:
+                if not end:
+                    if event.key == pygame.K_RETURN:
+                        screen.end_time = time.time()
+                        if len(screen.input) == 0:
+                            screen.words = 1
+                        else:
+                            for i in screen.input:
+                                if i == ' ':
+                                    screen.words += 1
+                        end = True
+                    elif event.key == pygame.K_BACKSPACE:
+                        screen.input = screen.input[:-1]
+                    else:
+                        if screen.input == '':
+                            screen.start_time = time.time()
+                        screen.input += event.unicode
+            display.fill(0)
+            sentence_input, xy_sentence = screen.center_text(screen.sentence, 200)
+            display.blit(sentence_input, xy_sentence)
+            text_input, xy_text = screen.center_text(screen.input, 300)
+            display.blit(text_input, xy_text)
+            if end == False:
+                start_text, xy_start = screen.center_text("Start typing!", 250)
+                display.blit(start_text, xy_start)
+            if end == True:
+                pygame.draw.rect(display, blue, pygame.Rect(325, 385, 100, 100))
+                playagain_text, xy_playagain = screen.center_text("Click reset to play again", 250)
+                display.blit(playagain_text, xy_playagain)
+                reset, xy_reset = screen.center_text("Reset", 435)
+                display.blit(reset, xy_reset)
+                result = screen.results()
+                text_result, xy_result = screen.center_text(result, 350)
+                display.blit(text_result, xy_result)
+            pygame.display.flip()
 
 if __name__ == "__main__":
     main()
--- src/speedtyping.py
+++ src/speedtyping.py
@@ -1,13 +1,13 @@
-import pygame
-import time
 import random
+import pygame
 
 class SpeedTyping:
     def __init__(self):
-        pass
-
-    def draw_text(self):
-        pass
+        self.start_time = 0
+        self.end_time = 0
+        self.sentence = self.get_sentence()
+        self.input = ''
+        self.words = 1
 
     def get_sentence(self):
         # the file won't open without src/ for me, don't know if 
@@ -16,3 +16,31 @@ class SpeedTyping:
         sentences = file.split("\n")
         pick = random.choice(sentences)
         return pick
+
+    def results(self):
+        timer = self.end_time - self.start_time
+        input_counter = 0
+        counter = 0
+        for i in self.sentence:
+            if len(self.input) > input_counter:
+                if self.input[input_counter] == i:
+                    counter += 1
+            input_counter += 1
+        n = len(self.sentence)
+        accuracy = (counter/n) * 100
+        wpm = len(self.input)*60/(self.words*timer)
+        return f"Time: {round(timer, 1)} s  Accuracy: {round(accuracy)} %  WPM: {round(wpm)}"
+
+    def center_text(self, text, y):
+        font = pygame.font.SysFont("Times New Roman", 24)
+        txt = font.render(text, True, (255,255,255))
+        rect = txt.get_rect(center=(750/2, y))
+        return txt, rect
+
+    def reset(self):
+        self.start_time = 0
+        self.end_time = 0
+        self.sentence = self.get_sentence()
+        self.input = ''
+        self.words = 1
+
